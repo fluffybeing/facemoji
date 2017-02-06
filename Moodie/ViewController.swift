2ba2a6b17fb62db7baeee04aa06ea94769d0ab2e
Rahul Ranjan <rahul.ranjan@udacity.com>
2017-02-06 16:44:59 +0530

    add different emotions emojis images

--- Moodie/ViewController.swift
+++ Moodie/ViewController.swift
@@ -11,13 +11,24 @@ import Cocoa
 
 class ViewController: NSViewController, AFDXDetectorDelegate {
     
+    @IBOutlet weak var imageLabel: NSTextField!
+    @IBOutlet weak var imageView: NSImageView!
+    
     var detector: AFDXDetector!
+    var emojiView: NSImageView!
+    
+    // facepoint can be nil
+    var facePoints: NSArray?
+    var imageBound: CGRect?
     
     override func viewWillAppear() {
         super.viewWillAppear()
         
         // launch the detector
         createDetector()
+        
+        // add subviews
+        self.addEmojiView()
     }
     
     override func viewWillDisappear() {
@@ -28,13 +39,13 @@ class ViewController: NSViewController, AFDXDetectorDelegate {
     
     
     func createDetector() {
-        detector = AFDXDetector.init(delegate: self, using: AFDX_CAMERA_FRONT, maximumFaces: 1)
+        detector = AFDXDetector.init(delegate: self, using: AFDX_CAMERA_FRONT, maximumFaces: 5)
         // detector specific settings
         // http://developer.affectiva.com/v3_1_1/osx/analyze-camera/
-        detector.maxProcessRate = 8.0;
-        detector.setDetectAllEmotions(true)
+        detector.maxProcessRate = 2.0;
+        // detector.setDetectAllEmotions(true)
+        // detector.setDetectAllExpressions(true)
         detector.setDetectEmojis(true)
-        detector.setDetectAllExpressions(true)
         
         let error = detector.start()
         
@@ -56,23 +67,87 @@ class ViewController: NSViewController, AFDXDetectorDelegate {
     // delegates methods
     func detectorDidStartDetectingFace(face : AFDXFace) {
         // handle new face
+        print("New Face Detected!")
     }
     
     func detectorDidStopDetectingFace(face : AFDXFace) {
         // handle loss of existing face
+        print("Stopped Face Detection!")
     }
     
+    
+    func rectangleBound(points: NSArray) {
+        
+        // This line literally killed me
+        // two level type casting
+        // let point = (points[0] as! NSValue) as CGPoint
+        let points = points.map{$0 as! CGPoint}
+        
+        // This will give the bounds as the Affdex API don't have face bounds
+        let minX = points.sorted(by: {$0.x < $1.x})[0].x
+        let maxX = points.sorted(by: {$0.x > $1.x})[0].x
+        let minY = points.sorted(by: {$0.y < $1.y})[0].y
+        let maxY = points.sorted(by: {$0.y > $1.y})[0].y
+        
+        self.imageBound = CGRect(x: minX, y: minY, width: (maxX-minX), height: (maxY-minY))
+    }
+    
+    func addEmojiView() {
+        self.emojiView = NSImageView(frame: self.imageBound ?? CGRect())
+        self.imageView.addSubview(emojiView)
+    }
+    
+    func addEmojiImage(emotion: String) {
+        self.emojiView.frame = self.imageBound ?? CGRect()
+        let image  = NSImage(named: emotion)
+        emojiView.image = image!
+    }
+    
+    func removeEmojiImage() {
+        self.emojiView.image = nil
+    }
     
     func detector(_ detector: AFDXDetector!, hasResults faces: NSMutableDictionary!, for image: NSImage!, atTime time: TimeInterval) {
         
         if faces != nil {
             // enumrate the dictionary of faces and process each one
-            for (_, face) in faces! {
-                let emoji : AFDXEmoji = (face as AnyObject).emojis
-                print(emoji)
+            for (_, face ) in faces! {
+                
+                // set the face point
+                self.facePoints = (face as AnyObject).facePoints
+                
+                let emoji = (face as AnyObject).emojis!.dominantEmoji
+                
+                switch emoji.rawValue {
+                case 128515:
+                    self.addEmojiImage(emotion: "smiley")
+                case 128518:
+                    self.addEmojiImage(emotion: "laughing")
+                case 128535:
+                    self.addEmojiImage(emotion: "kissing")
+                case 128545:
+                    self.addEmojiImage(emotion: "rage")
+                case 128561:
+                    self.addEmojiImage(emotion: "scream")
+                case 128539:
+                    self.addEmojiImage(emotion: "tongue")
+                case 9786:
+                    self.addEmojiImage(emotion: "relaxed")
+                default:
+                    self.removeEmojiImage()
+                }
             }
         } else {
             // handle unprocessed image in this block of code
+            // get the rectangle bounds for face
+            if facePoints != nil {
+                // calculate face bound
+                self.rectangleBound(points: facePoints!)
+                
+                let rectImage = AFDXDetector.image(byDrawingPoints: nil, andRectangles: nil, andImages: nil, withRadius: 2.0, usingPointColor: NSColor.white, usingRectangleColor: NSColor.green, usingImageRects: nil, on: image)
+            
+                self.imageView.image = rectImage
+            }
         }
     }
 }
